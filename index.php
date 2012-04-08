2d331597823106cd03d0c9ad448532f8df0b898d
Astianax <alyahir09@gmail.com>
2012-04-08 14:43:04 -0300

    Prueba del pull de jairo

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ include 'Entity/PersonalUserEO.php';
     $ifSend = true;
     $message="";
     $messageLogin="";
-    //Jairo Pavel Veloz Vidal
     if(isset($_POST['btnRegistrame'])){
         $personalUser->userName=mysql_real_escape_string(trim($_POST['txtName']));
         $personalUser->email=mysql_real_escape_string(trim($_POST['txtEmail']));
