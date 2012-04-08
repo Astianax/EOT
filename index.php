59f9144161aaec412add7f82dd5c385b4600bd56
Astianax <alyahir09@gmail.com>
2012-04-08 03:31:20 -0300

    modificacion de para ponerle 0 alante al valor de la fecha

--- index.php
+++ index.php
@@ -161,57 +161,24 @@ include 'Entity/PersonalUserEO.php';
 					<input type="radio" name="rbnGender" value="Hombre" id="male" clas="sexReg" />
 					<label for="male" class="sexReg"> Hombre</label>
 				</div>
-                                        <?php
-                                            $fechaInicio="1910";
-                                            $fechaFin= "1914";
-                                            
-                                                for($i=$fechaInicio; $i<=$fechaFin; $i++){
-                                                    $year=substr($personalUser->birthday, 0,4);
-                                                    if($year==$i){                                                                                                          
-                                                    echo"<option value='{$i}' selected> ".$i."</option>";                                                 
-                                                    }else {
-                                                         echo"<option value='{$i}'> ".$i."</option>";                                                                                                         
-                                                    }
-                                                    
-                                                }
-                                                                                   
-                                               ?>
                                 
 				<div class="birthDate"><strong>Fecha de Nacimiento</strong></div>
 				<div>
-					<select name="day">
-                                            <option value="00" >Día:</option>
-						<option value="01">1</option>
-						<option value="02">2</option>
-						<option value="03">3</option>
-						<option value="04">4</option>
-						<option value="05">5</option>
-						<option value="06">6</option>
-						<option value="07">7</option>
-						<option value="08">8</option>
-						<option value="09">9</option>
-						<option value="10">10</option>
-						<option value="11">11</option>
-						<option value="12">12</option>
-						<option value="13">13</option>
-						<option value="14">14</option>
-						<option value="15">15</option>
-						<option value="16">16</option>
-						<option value="17">17</option>
-						<option value="18">18</option>
-						<option value="19">19</option>
-						<option value="20">20</option>
-						<option value="21">21</option>
-						<option value="22">22</option>
-						<option value="23">23</option>
-						<option value="24">24</option>
-						<option value="25">25</option>
-						<option value="26">26</option>
-						<option value="27">27</option>
-						<option value="28">28</option>
-						<option value="29">29</option>
-						<option value="30">30</option>
-						<option value="31">31</option>
+                                 <?php
+                                    $fechaInicio="1";
+                                    $fechaFin= "31";
+
+                                        for($i=$fechaInicio; $i<=$fechaFin; $i++){
+                                            $day=substr($personalUser->birthday, 6,8);
+                                            if($day==$i && $i<=9 ){                                                                                                          
+                                            echo"<option value='0{$i}' selected> ".$i."</option>";                                                 
+                                            }else if($day==$i && $i>9){
+                                                echo"<option value='{$i}' selected> ".$i."</option>"; 
+                                            }else if($day!=$i || $i>9 ){                                                     
+                                                echo"<option value='{$i}'> ".$i."</option>";   
+                                            }
+                                        }                                                                                   
+                                        ?>
 					</select>
 					<select name="month">
 						<option value="00">Mes:</option>
@@ -234,7 +201,7 @@ include 'Entity/PersonalUserEO.php';
                                             $fechaInicio="1910";
                                             $fechaFin= "2012";
                                             
-                                                for($i=$fechaInicio; $i<=$fechaFin; $i--){
+                                                for($i=$fechaInicio; $i<=$fechaFin; $i++){
                                                     $year=substr($personalUser->birthday, 0,4);
                                                     if($year==$i){                                                                                                          
                                                     echo"<option value='{$i}' selected> ".$i."</option>";                                                 
