69f628c92431d710176e91301addbd154184d6fb
Astianax <alyahir09@gmail.com>
2012-04-08 16:38:18 -0300

    Modificacion del array de jairo

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     $ifSend = true;
     $message="";
     $messageLogin="";
-    $userDate = array("","","");mmm
+    $userDate = array("","","");
     if(isset($_POST['btnRegistrame'])){
         $personalUser->userName=mysql_real_escape_string(trim($_POST['txtName']));
         $personalUser->email=mysql_real_escape_string(trim($_POST['txtEmail']));
