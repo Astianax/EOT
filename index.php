59b5fbb6b87be2f42ee559e555ed6fefb41c222b
Astianax <alyahir09@gmail.com>
2012-04-08 03:08:06 -0300

    Haciendo el rango de los anos

--- index.php
+++ index.php
@@ -147,11 +147,11 @@ include 'Entity/PersonalUserEO.php';
 				<h2>Registrate!</h2>
 				<span class="descReg">Forma parte de <b>EOTrailer</b> y disfruta con tus amigos!</span>
 				<label for="textName"><b>Nombre</b></label>
-                                <input type="text" id="textName" class="textReg border-radius10" name="txtName"/>
+                                <input type="text" id="textName" class="textReg border-radius10" name="txtName" value="<?php echo $personalUser->userName; ?>"/>
 				<label for="textEmail"><b>Correo Electrónico</b></label>
-				<input type="text" id="textEmail" class="textReg border-radius10" name="txtEmail"/>
+				<input type="text" id="textEmail" class="textReg border-radius10" name="txtEmail" value="<?php echo $personalUser->email; ?>"/>
 				<label for="textPass"><b>Introduzca su contraseña</b></label>
-				<input type="password" id="textPass" class="textReg border-radius10" name="txtPassword"/>
+				<input type="password" id="textPass" class="textReg border-radius10" name="txtPassword" />
 				<label for="textPassAgain"><b>Introduzca de nuevo su contraseña</b>
 				<input type="password" id="textPassAgain" class="textReg border-radius10" name="txtPassAgain"/>
                                 </label>
@@ -161,10 +161,26 @@ include 'Entity/PersonalUserEO.php';
 					<input type="radio" name="rbnGender" value="Hombre" id="male" clas="sexReg" />
 					<label for="male" class="sexReg"> Hombre</label>
 				</div>
+                                        <?php
+                                            $fechaInicio="1910";
+                                            $fechaFin= "1914";
+                                            
+                                                for($i=$fechaInicio; $i<=$fechaFin; $i++){
+                                                    $year=substr($personalUser->birthday, 0,4);
+                                                    if($year==$i){                                                                                                          
+                                                    echo"<option value='{$i}' selected> ".$i."</option>";                                                 
+                                                    }else {
+                                                         echo"<option value='{$i}'> ".$i."</option>";                                                                                                         
+                                                    }
+                                                    
+                                                }
+                                                                                   
+                                               ?>
+                                
 				<div class="birthDate"><strong>Fecha de Nacimiento</strong></div>
 				<div>
 					<select name="day">
-						<option value="00">Día:</option>
+                                            <option value="00" >Día:</option>
 						<option value="01">1</option>
 						<option value="02">2</option>
 						<option value="03">3</option>
@@ -213,115 +229,20 @@ include 'Entity/PersonalUserEO.php';
 						<option value="12">diciembre</option>
 					</select>
 					<select name="year">
-						<option value="0000">Año:</option>
-						<option value="2012">2012</option>
-						<option value="2011">2011</option>
-						<option value="2010">2010</option>
-						<option value="2009">2009</option>
-						<option value="2008">2008</option>
-						<option value="2007">2007</option>
-						<option value="2006">2006</option>
-						<option value="2005">2005</option>
-						<option value="2004">2004</option>
-						<option value="2003">2003</option>
-						<option value="2002">2002</option>
-						<option value="2001">2001</option>
-						<option value="2000">2000</option>
-						<option value="1999">1999</option>
-						<option value="1998">1998</option>
-						<option value="1997">1997</option>
-						<option value="1996">1996</option>
-						<option value="1995">1995</option>
-						<option value="1994">1994</option>
-						<option value="1993">1993</option>
-						<option value="1992">1992</option>
-						<option value="1991">1991</option>
-						<option value="1990">1990</option>
-						<option value="1989">1989</option>
-						<option value="1988">1988</option>
-						<option value="1987">1987</option>
-						<option value="1986">1986</option>
-						<option value="1985">1985</option>
-						<option value="1984">1984</option>
-						<option value="1983">1983</option>
-						<option value="1982">1982</option>
-						<option value="1981">1981</option>
-						<option value="1980">1980</option>
-						<option value="1979">1979</option>
-						<option value="1978">1978</option>
-						<option value="1977">1977</option>
-						<option value="1976">1976</option>
-						<option value="1975">1975</option>
-						<option value="1974">1974</option>
-						<option value="1973">1973</option>
-						<option value="1972">1972</option>
-						<option value="1971">1971</option>
-						<option value="1970">1970</option>
-						<option value="1969">1969</option>
-						<option value="1968">1968</option>
-						<option value="1967">1967</option>
-						<option value="1966">1966</option>
-						<option value="1965">1965</option>
-						<option value="1964">1964</option>
-						<option value="1963">1963</option>
-						<option value="1962">1962</option>
-						<option value="1961">1961</option>
-						<option value="1960">1960</option>
-						<option value="1959">1959</option>
-						<option value="1958">1958</option>
-						<option value="1957">1957</option>
-						<option value="1956">1956</option>
-						<option value="1955">1955</option>
-						<option value="1954">1954</option>
-						<option value="1953">1953</option>
-						<option value="1952">1952</option>
-						<option value="1951">1951</option>
-						<option value="1950">1950</option>
-						<option value="1949">1949</option>
-						<option value="1948">1948</option>
-						<option value="1947">1947</option>
-						<option value="1946">1946</option>
-						<option value="1945">1945</option>
-						<option value="1944">1944</option>
-						<option value="1943">1943</option>
-						<option value="1942">1942</option>
-						<option value="1941">1941</option>
-						<option value="1940">1940</option>
-						<option value="1939">1939</option>
-						<option value="1938">1938</option>
-						<option value="1937">1937</option>
-						<option value="1936">1936</option>
-						<option value="1935">1935</option>
-						<option value="1934">1934</option>
-						<option value="1933">1933</option>
-						<option value="1932">1932</option>
-						<option value="1931">1931</option>
-						<option value="1930">1930</option>
-						<option value="1929">1929</option>
-						<option value="1928">1928</option>
-						<option value="1927">1927</option>
-						<option value="1926">1926</option>
-						<option value="1925">1925</option>
-						<option value="1924">1924</option>
-						<option value="1923">1923</option>
-						<option value="1922">1922</option>
-						<option value="1921">1921</option>
-						<option value="1920">1920</option>
-						<option value="1919">1919</option>
-						<option value="1918">1918</option>
-						<option value="1917">1917</option>
-						<option value="1916">1916</option>
-						<option value="1915">1915</option>
-						<option value="1914">1914</option>
-						<option value="1913">1913</option>
-						<option value="1912">1912</option>
-						<option value="1911">1911</option>
-						<option value="1910">1910</option>
-						<option value="1909">1909</option>
-						<option value="1908">1908</option>
-						<option value="1907">1907</option>
-						<option value="1906">1906</option>
-						<option value="1905">1905</option>
+                                           <option value="0000">Año:</option>
+                                           <?php
+                                            $fechaInicio="1910";
+                                            $fechaFin= "2012";
+                                            
+                                                for($i=$fechaInicio; $i<=$fechaFin; $i--){
+                                                    $year=substr($personalUser->birthday, 0,4);
+                                                    if($year==$i){                                                                                                          
+                                                    echo"<option value='{$i}' selected> ".$i."</option>";                                                 
+                                                    }else {
+                                                         echo"<option value='{$i}'> ".$i."</option>";                                                                                                         
+                                                    }
+                                                }                                                                                   
+                                               ?>
 					</select>
 				</div>
 				<div class="privateRegText"> Al hacer clic en <strong>"Regístrate"</strong> estas de acuerdo con nuestras condiciones y aceptas haber leído y comprendido nuestra <a href="#">Política de Privacidad</a> </div>
